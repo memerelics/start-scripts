4eb42be448509cf5682f272e05a6a7b40c007267
takuyahashimoto <azuraubade.nyx21@gmail.com>
2010-12-16 22:27:26 +0900

    modify practical install git

--- installGitOnCentOS.sh
+++ installGitOnCentOS.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
+pwd
 cd /usr/local/src
-wget http://kernel.org/pub/software/scm/git/git-1.7.3.tar.bz2
-#replace latest version here
-tar xvjf git-1.7.3.tar.bz2
+pwd
+sudo wget http://kernel.org/pub/software/scm/git/git-1.7.3.tar.bz2
+
+sudo tar xvjf git-1.7.3.tar.bz2
 cd git-1.7.3
-yum -y install zlib-devel
-./configure --prefix=/usr/local/
-make
-make install
+pwd
+sudo yum -y install zlib-devel
+sudo ./configure --prefix=/usr/local/
+sudo make
+sudo make install
 git config --global user.name "takuyahashimoto"
 git config --global user.email takuya21hashimoto@gmail.com
