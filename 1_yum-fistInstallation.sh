05c373c3167b251da9043f5e55403dcdca28e94f
takuyahashimoto <takuya21hashimoto@gmail.com>
2011-01-16 01:05:28 +0900

    separate yums not needed for first server setup

--- 1_yum-fistInstallation.sh
+++ 1_yum-fistInstallation.sh
@@ -10,14 +10,3 @@ fi
 yum -y update
 yum -y upgrade
 
-### needed for youseikouza
-#sudo yum -y install telnet-server
-#sudo yum -y install postfix
-#sudo yum -y install dovecot
-#sudo yum -y install cyrus-imapd
-#sudo yum -y install vsftpd
-
-#sudo yum -y install samba-client
-#sudo yum -y install bind
-#sudo yum -y install bind-chroot
-#sudo yum -y install caching-nameserver
