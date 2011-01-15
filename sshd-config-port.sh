e94c6ca0d90d27e19ab111e1c51cd4542c2760a8
takuyahashimoto <takuya21hashimoto@gmail.com>
2011-01-16 00:17:13 +0900

    add arguments check

--- sshd-config-port.sh
+++ sshd-config-port.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# if the number of arguments != 1
+if [ $# -ne 1 ]; then
+    echo "need to define new port"
+    exit 1
+fi
+
 sudo cp -p /etc/ssh/sshd_config /etc/ssh/sshd_config.original
-sudo sed "/Port 22/a Port 15535" /etc/ssh/sshd_config.original > /etc/ssh/sshd_config
+sudo sed "/Port 22/a Port ${1}" /etc/ssh/sshd_config.original > /etc/ssh/sshd_config
 sudo /etc/rc.d/init.d/sshd restart
