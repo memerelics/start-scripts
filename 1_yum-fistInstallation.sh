e77f22619da8dcb202c54e5da45614e4c88402ce
takuyahashimoto <takuya21hashimoto@gmail.com>
2011-01-16 01:09:20 +0900

    merge 1 and 3

--- 1_yum-fistInstallation.sh
+++ 1_yum-fistInstallation.sh
@@ -15,5 +15,24 @@ useradd hash
 passwd hash
 usermod -G wheel hash
 visudo
+
 # remove comment of wheel group
 # and may need to Path to /usr/sbin
+
+
+### run as hash -- because create new file/dirs ###
+su hash
+### run as hash ###
+
+sudo yum -y install zlib-devel
+
+mkdir -p /home/hash/work/src
+cd /home/hash/work/src
+sudo wget http://kernel.org/pub/software/scm/git/git-1.7.3.tar.bz2
+sudo tar xvjf git-1.7.3.tar.bz2
+
+cd git-1.7.3
+
+sudo ./configure --prefix=/usr/local
+sudo make
+sudo make install
