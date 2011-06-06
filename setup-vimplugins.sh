2617e5e132cc08211f278de0c544d124adb7bcd0
takuyahashimoto <takuya21hashimoto@gmail.com>
2011-06-06 15:58:46 +0900

    manage some plugins

--- setup-vimplugins.sh
+++ setup-vimplugins.sh
@@ -22,7 +22,7 @@ git submodule add git://github.com/Shougo/vimshell.git bundle/vimshell.vim
 cd $HOME/.vim/bundle/vimproc.vim
 make -f make_gcc.mak
 
-### copy to runtime.... ??
+# copy to runtime.... ??
 # say different words in English/Japanese
 # vimproc.txt:  After compile, you copy autoload/* files to your |$VIMRUNTIME|.
 # vimproc.jat: コンパイルが通ったら、autoload/にあるファイルやディレクトリを.vimのautoloadディレクトリにコピーする。
@@ -32,13 +32,16 @@ cp -r autoload/* $HOME/.vim/autoload/
 
 
 ### surround.vim
-#cd $HOME/.vim
-#wget http://vim.sourceforge.net/scripts/download_script.php?src_id=12566 -O surround.vim.zip
+cd $HOME/.vim
+wget http://vim.sourceforge.net/scripts/download_script.php?src_id=12566 -O surround.vim.zip
+mkdir -p bundle/surround.vim
+mv surround.vim.zip bundle/surround.vim
+cd bundle/surround.vim && unzip surround.vim.zip
 
 ### Align.vim
-#cd $HOME/.vim
-#git submodule add git://github.com/tsaleh/vim-align.git bundle/align.git
+cd $HOME/.vim
+git submodule add git://github.com/tsaleh/vim-align.git bundle/align.git
 
 ### rails.vim
-#cd $HOME/.vim
-#git submodule add git://github.com/tpope/vim-rails.git bundle/rails.git
+cd $HOME/.vim
+git submodule add git://github.com/tpope/vim-rails.git bundle/rails.git
