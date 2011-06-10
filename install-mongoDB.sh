eddce770be13616257f8226ad37e7c6c210ceed1
takuyahashimoto <takuya21hashimoto@gmail.com>
2011-06-11 01:54:08 +0900

    mongodb bin

--- install-mongoDB.sh
+++ install-mongoDB.sh
@@ -13,8 +13,12 @@ fi
 cd $TARGET_DIR
 curl http://fastdl.mongodb.org/linux/mongodb-linux-i686-1.8.1.tgz > mongo.tgz
 tar xzf mongo.tgz
+mv mongodb-linux-i686-1.8.1 mongo
+export PATH=$PATH:$HOME/work/src/mongo/bin
 
 sudo mkdir -p /data/db/
 sudo chown `id -u` /data/db
 
-#TODO: where to set bin files?
+### check installation
+# % mongod &
+# % mongo
