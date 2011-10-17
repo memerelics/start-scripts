1355570a1554f41d73399f4449812ef301c1bf5a
takuyahashimoto <takuya21hashimoto@gmail.com>
2011-10-18 06:28:34 +0900

    インストール時にVundle設定までさせるようにした

--- install/install-vim73.sh
+++ install/install-vim73.sh
@@ -9,12 +9,12 @@ fi
 ### install required yums
 sudo yum -y install patch
 sudo yum -y install ncurses-devel
-sudo yum -y install libselinux-devel # for selinux 
+sudo yum -y install libselinux-devel # for selinux
 
 
 mkdir -p /home/hash/work/src
 
-### KEEP cd 
+### KEEP cd
 cd /home/hash/work/src
 echo "cd /home/hash/work/src\n"
 
@@ -45,3 +45,16 @@ sudo make install
 ### ERROR
 #  make[1]: *** [objects/os_unix.o] Error 1
 # => added [sudo yum -y install libselinux-devel]
+
+
+##############################
+#     Vundle settings        #
+##############################
+
+mkdir -p ~/.vim/bundle
+git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+#  => fatal: Unable to find remote helper for 'http'
+# if here you face "no http" error, it's better to re-install git.
+vim "+BundleInstall" "+qall"
+
+
