5193bc5425d88316f97a7bdea14d62fdf3af8016
takuyahashimoto <takuya21hashimoto@gmail.com>
2011-11-24 12:15:03 +0900

    update rvm installation

--- install/install-ruby-by-rvm.sh
+++ install/install-ruby-by-rvm.sh
@@ -1,6 +1,6 @@
 bash < <(curl -s https://rvm.beginrescueend.com/install/rvm)
-rvm install ruby-1.9.2-p180
-rvm 1.9.2
+rvm install ruby-1.9.2
+rvm use 1.9.2
 rvm info
 which ruby
-
+cat ~/.bashrc | grep rvm
