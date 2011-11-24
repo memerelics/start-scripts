dde210e60642f96619fe134f19d5e0acea49c6e5
takuyahashimoto <takuya21hashimoto@gmail.com>
2011-11-24 12:18:05 +0900

    rename

--- install/install-ruby-by-rvm.sh
+++ install/install-ruby-by-rvm.sh
@@ -1,6 +1,25 @@
-bash < <(curl -s https://rvm.beginrescueend.com/install/rvm)
-rvm install ruby-1.9.2
-rvm use 1.9.2
-rvm info
-which ruby
-cat ~/.bashrc | grep rvm
+#!/usr/bash
+
+# when .bashrc exists, rvm-installer automatically add source rvm script into it.
+if [ ! -e $HOME/.bashrc ]; then
+  touch $HOME/.bashrc
+fi
+
+curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer > tmp-rvm-installer
+bash tmp-rvm-installer
+rm tmp-rvm-installer
+
+# source to make rvm command available
+source ~/.bashrc
+
+# install ruby and use it
+rvm install 1.9.2-p290
+echo "rvm use 1.9.2-p290" >> ~/.bashrc
+source ~/.bashrc
+
+echo " ---------- Ruby Version ------------ "
+ruby --version
+echo " ------------------------------------ "
+
+# gem
+gem install bundler --no-rdoc --no-ri
